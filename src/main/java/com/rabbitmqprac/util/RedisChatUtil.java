276fe560e1a067f1a652cfc838fdb9de9f86082a
seungheonlee <lsh2613@naver.com>
2025-04-12 00:38:46 +0900

    refactor: 변수명 수정

--- src/main/java/com/rabbitmqprac/util/RedisChatUtil.java
+++ src/main/java/com/rabbitmqprac/util/RedisChatUtil.java
@@ -3,35 +3,33 @@ package com.rabbitmqprac.util;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.SetOperations;
-import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
 
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Component
 public class RedisChatUtil {
 
-    private final RedisTemplate<Long, Long> chatRoom2Members;
+    private final RedisTemplate<Long, Long> redisTemplate;
 
     public void addChatRoom2Member(Long chatRoomId, Long memberId) {
-        SetOperations<Long, Long> ops = chatRoom2Members.opsForSet();
+        SetOperations<Long, Long> ops = redisTemplate.opsForSet();
         ops.add(chatRoomId, memberId);
     }
 
     public Set<Long> getOnlineMembers(Long chatRoomId) {
-        SetOperations<Long, Long> ops = chatRoom2Members.opsForSet();
+        SetOperations<Long, Long> ops = redisTemplate.opsForSet();
         return ops.members(chatRoomId);
     }
 
     public int getOnlineMemberCntInChatRoom(Long chatRoomId) {
-        SetOperations<Long, Long> ops = chatRoom2Members.opsForSet();
+        SetOperations<Long, Long> ops = redisTemplate.opsForSet();
         return ops.members(chatRoomId).size();
     }
 
     public void removeChatRoom2Member(Long chatRoomId, Long memberId) {
-        SetOperations<Long, Long> ops = chatRoom2Members.opsForSet();
+        SetOperations<Long, Long> ops = redisTemplate.opsForSet();
         ops.remove(chatRoomId, memberId);
     }
 }
