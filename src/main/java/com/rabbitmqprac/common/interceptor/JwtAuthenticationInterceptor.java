a58205089a4d5bd562e7dad645a983d4afccde65
seungheonlee <lsh2613@naver.com>
2025-04-12 21:36:27 +0900

    fix: switch NPE 에러 해결

--- src/main/java/com/rabbitmqprac/common/interceptor/JwtAuthenticationInterceptor.java
+++ src/main/java/com/rabbitmqprac/common/interceptor/JwtAuthenticationInterceptor.java
@@ -5,12 +5,15 @@ import com.rabbitmqprac.common.constant.TokenType;
 import com.rabbitmqprac.util.StompHeaderAccessorUtil;
 import com.rabbitmqprac.util.TokenUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationInterceptor implements ChannelInterceptor {
@@ -22,11 +25,12 @@ public class JwtAuthenticationInterceptor implements ChannelInterceptor {
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+        StompCommand command = accessor.getCommand();
 
-        switch (accessor.getCommand()) {
-            case CONNECT -> handleConnect(accessor);
-            case DISCONNECT -> handleDisconnect(accessor);
-        }
+        if (command==StompCommand.CONNECT)
+            handleConnect(accessor);
+        if (command==StompCommand.DISCONNECT)
+            handleDisconnect(accessor);
 
         return message;
     }
