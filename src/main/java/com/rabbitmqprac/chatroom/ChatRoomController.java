dc49e4d8e6f95c9d59e4e86d4d112e505f7e753a
seungheonlee <lsh2613@naver.com>
2025-04-14 15:35:59 +0900

    refactor: ChatRoomController를 RESTful에서 MVC로 변경하여 뷰 반환

--- src/main/java/com/rabbitmqprac/chatroom/ChatRoomController.java
+++ src/main/java/com/rabbitmqprac/chatroom/ChatRoomController.java
@@ -6,11 +6,16 @@ import com.rabbitmqprac.chatroom.dto.ChatRoomRes;
 import com.rabbitmqprac.chatroom.dto.MyChatRoomRes;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 @Slf4j
 public class ChatRoomController {
@@ -18,17 +23,23 @@ public class ChatRoomController {
     private final ChatRoomService chatRoomService;
 
     @PostMapping("/chat-rooms")
-    public ChatRoomCreateRes createChatRoom(@RequestBody ChatRoomCreateReq chatRoomCreateReq) {
-        return chatRoomService.createChatRoom(chatRoomCreateReq);
+    public String createChatRoom(@RequestBody ChatRoomCreateReq chatRoomCreateReq, Model model) {
+        ChatRoomCreateRes chatRoomCreateRes = chatRoomService.createChatRoom(chatRoomCreateReq);
+        model.addAttribute("chatRoomCreateRes", chatRoomCreateRes);
+        return "chat-room-create";
     }
 
     @GetMapping("/chat-rooms")
-    public List<MyChatRoomRes> getMyChatRooms(@RequestParam Long loginId) {
-        return chatRoomService.getMyChatRooms(loginId);
+    public String getMyChatRooms(@RequestParam Long loginId, Model model) {
+        List<MyChatRoomRes> myChatRooms = chatRoomService.getMyChatRooms(loginId);
+        model.addAttribute("myChatRooms", myChatRooms);
+        return "my-chat-rooms";
     }
 
     @GetMapping("/chat-rooms/all")
-    public List<ChatRoomRes> getChatRooms() {
-        return chatRoomService.getChatRooms();
+    public String getChatRooms(Model model) {
+        List<ChatRoomRes> chatRooms = chatRoomService.getChatRooms();
+        model.addAttribute("chatRooms", chatRooms);
+        return "chat-rooms";
     }
 }
