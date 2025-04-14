c5584ef57aa3525328627cb9c12e5b0e45c449ef
seungheonlee <lsh2613@naver.com>
2025-04-14 15:29:43 +0900

    feat: header 구현

---
+++ src/main/resources/static/js/header.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("header.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("header-placeholder").innerHTML = data;
+        })
+        .catch(error => console.error("헤더 로드 실패:", error));
+});
