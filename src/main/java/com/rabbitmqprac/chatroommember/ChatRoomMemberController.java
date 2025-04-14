9debe619aec2d75664e3f802ed72df1af222fb65
seungheonlee <lsh2613@naver.com>
2025-04-14 21:14:29 +0900

    refactor: ChatRoomMemberController를 RESTful에서 MVC로 변경하여 뷰 반환

--- src/main/java/com/rabbitmqprac/chatroommember/ChatRoomMemberController.java
+++ src/main/java/com/rabbitmqprac/chatroommember/ChatRoomMemberController.java
@@ -2,6 +2,7 @@ package com.rabbitmqprac.chatroommember;
 
 import com.rabbitmqprac.chatroommember.dto.ChatRoomMemberRes;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,7 +19,9 @@ public class ChatRoomMemberController {
     }
 
     @GetMapping("/chat-rooms/{chatRoomId}/members")
-    public List<ChatRoomMemberRes> getChatRoomMembers(@PathVariable Long chatRoomId) {
-        return chatRoomMemberService.getChatRoomMembers(chatRoomId);
+    public List<ChatRoomMemberRes> getChatRoomMembers(@PathVariable Long chatRoomId, Model model) {
+        List<ChatRoomMemberRes> chatRoomMembers = chatRoomMemberService.getChatRoomMembers(chatRoomId);
+        model.addAttribute("chatRoomMembers", chatRoomMembers);
+        return chatRoomMembers;
     }
 }
