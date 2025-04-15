e9192a6f6d0c17b8f241bdf95fc97f19d7c59770
seungheonlee <lsh2613@naver.com>
2025-04-16 02:56:51 +0900

    fix: RequesterArgumentResolver 등록

--- src/main/java/com/rabbitmqprac/common/config/WebConfig.java
+++ src/main/java/com/rabbitmqprac/common/config/WebConfig.java
@@ -1,11 +1,18 @@
 package com.rabbitmqprac.common.config;
 
+import com.rabbitmqprac.common.resolver.RequesterArgumentResolver;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    private final RequesterArgumentResolver requesterArgumentResolver;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -14,5 +21,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(false); // 쿠키 사용 안 하면 false, 사용하면 true
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(requesterArgumentResolver);
     }
 }
