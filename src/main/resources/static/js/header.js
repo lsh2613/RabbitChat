717208e19cf31e0412cbd9fe76d8293c42286a62
seungheonlee <lsh2613@naver.com>
2025-04-16 03:05:21 +0900

    refactor : 파일 경로 절대위치로 지정

--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("header.html")
+    fetch("/header.html")
         .then(response => response.text())
         .then(data => {
             document.getElementById("header-placeholder").innerHTML = data;
