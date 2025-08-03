c10c20bb5bf616ac7725d6f361cc79474d7b532e
seungheonlee <lsh2613@naver.com>
2025-08-04 01:16:39 +0900

    refactor: User Entity명 수정

--- src/main/java/com/rabbitmqprac/domain/persistence/user/entity/User.java
+++ src/main/java/com/rabbitmqprac/domain/persistence/user/entity/User.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Entity(name = "user")
+@Entity
 public class User extends DateAuditable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
