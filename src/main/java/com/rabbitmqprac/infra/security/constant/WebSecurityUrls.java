8ff62072f790f8c1d85ff36af02fd63e55bfc7f7
seungheonlee <lsh2613@naver.com>
2025-08-06 18:06:31 +0900

    refactor: public STOMP endpoint 추가

--- src/main/java/com/rabbitmqprac/infra/security/constant/WebSecurityUrls.java
+++ src/main/java/com/rabbitmqprac/infra/security/constant/WebSecurityUrls.java
@@ -5,5 +5,6 @@ public final class WebSecurityUrls {
     public static final String[] PUBLIC_ENDPOINTS = {"/"};
     public static final String[] ANONYMOUS_ENDPOINTS = {"/auth/sign-in", "/auth/sign-up"};
     public static final String[] AUTHENTICATED_ENDPOINTS = {"/"};
+    public static final String[] PUBLIC_STOMP_ENDPOINTS = {"/chat/inbox"};
     public static final String[] SWAGGER_ENDPOINTS = {"/api-docs/**", "/v3/api-docs/**", "/swagger-ui/**", "/swagger",};
 }
