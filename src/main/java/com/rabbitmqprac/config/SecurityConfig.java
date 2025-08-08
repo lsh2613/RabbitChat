3024228a3012ea8f6b9fa50c5aa941f668d5fdf7
seungheonlee <lsh2613@naver.com>
2025-08-09 03:31:58 +0900

    refactor: cors 적용

--- src/main/java/com/rabbitmqprac/config/SecurityConfig.java
+++ src/main/java/com/rabbitmqprac/config/SecurityConfig.java
@@ -21,7 +21,11 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -38,7 +42,7 @@ public class SecurityConfig {
     @Order(SecurityProperties.BASIC_AUTH_ORDER)
     public SecurityFilterChain filterChainDev(HttpSecurity http) throws Exception {
         return defaultSecurity(http)
-                .cors((cors) -> cors.configurationSource(corsConfigurationSource))
+                .cors((cors) -> cors.configurationSource(corsConfigurationSource()))
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(jwtExceptionFilter, JwtAuthenticationFilter.class)
                 .authorizeHttpRequests(
@@ -73,5 +77,19 @@ public class SecurityConfig {
 //                .requestMatchers(AUTHENTICATED_ENDPOINTS).authenticated()
                 .requestMatchers(WebSecurityUrls.PUBLIC_STOMP_ENDPOINTS).permitAll()
                 .requestMatchers(WebSecurityUrls.ANONYMOUS_ENDPOINTS).anonymous();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000"));
+        corsConfiguration.setAllowedHeaders(List.of("*"));
+        corsConfiguration.setExposedHeaders(List.of("Authorization"));
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        corsConfiguration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
     }
 }
