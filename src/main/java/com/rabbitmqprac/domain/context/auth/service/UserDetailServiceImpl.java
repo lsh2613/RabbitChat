a7cec3277d32711a4888cc411edaab857d9734e2
seungheonlee <lsh2613@naver.com>
2025-08-30 03:52:58 +0900

    chore: prometheus, grafana 적용
    - docker-compose 서비스(prometheus, grafana) 설정
    - metric 수집 endpoint에 대한 basic auth 설정

--- src/main/java/com/rabbitmqprac/domain/context/auth/service/UserDetailServiceImpl.java
+++ src/main/java/com/rabbitmqprac/domain/context/auth/service/UserDetailServiceImpl.java
@@ -1,15 +1,17 @@
 package com.rabbitmqprac.domain.context.auth.service;
 
-import com.rabbitmqprac.infra.security.authentication.SecurityUserDetails;
 import com.rabbitmqprac.domain.context.user.service.UserService;
 import com.rabbitmqprac.domain.persistence.user.entity.User;
+import com.rabbitmqprac.infra.security.authentication.SecurityUserDetails;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@Primary
 @RequiredArgsConstructor
 public class UserDetailServiceImpl implements UserDetailsService {
     private final UserService userService;
