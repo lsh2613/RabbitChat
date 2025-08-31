9d9155b71c5b58060a05f7ced9412719d86796af
LSH <104637774+lsh2613@users.noreply.github.com>
2025-08-31 15:53:52 +0900

    Chore: update init sql (#35)
    * chore: chat_message_status 테이블 삭제 및 AUTO_INCREMENT 초기화 추가
    
    * chore: 초기 데이터 샘플 변경 및 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -13,12 +13,16 @@ DELETE
 FROM `chat_message`;
 DELETE
 FROM `oauth`;
+DELETE
+FROM `chat_message_status`;
+
 -- AUTO_INCREMENT 값 초기화
 ALTER TABLE `user` AUTO_INCREMENT = 1;
 ALTER TABLE `chat_room` AUTO_INCREMENT = 1;
 ALTER TABLE `chat_room_member` AUTO_INCREMENT = 1;
 ALTER TABLE `chat_message` AUTO_INCREMENT = 1;
 ALTER TABLE `oauth` AUTO_INCREMENT = 1;
+ALTER TABLE `chat_message_status` AUTO_INCREMENT = 1;
 
 -- 빠른 삽입을 위해 검사 비활성화
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -39,16 +43,18 @@ LOCK
 TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
 INSERT INTO `user` (`user_id`, `username`, `password`, `nickname`, `role`, `created_at`, `updated_at`)
-VALUES (1, 'user_1', '$2a$10$gcUz6PGJLUrSt1TF6/rnc.FLqJPzevnkzDx2yY2Y49ehhuAxhQ4WW', 'USER_1', 'USER', NOW(), NOW()),
-       (2, 'user_2', '$2a$10$gcUz6PGJLUrSt1TF6/rnc.FLqJPzevnkzDx2yY2Y49ehhuAxhQ4WW', 'USER_2', 'USER', NOW(), NOW()),
-       (3, 'user_3', '$2a$10$gcUz6PGJLUrSt1TF6/rnc.FLqJPzevnkzDx2yY2Y49ehhuAxhQ4WW', 'USER_3', 'USER', NOW(), NOW()),
-       (4, 'user_4', '$2a$10$gcUz6PGJLUrSt1TF6/rnc.FLqJPzevnkzDx2yY2Y49ehhuAxhQ4WW', 'USER_4', 'USER', NOW(), NOW()),
-       (5, 'user_5', '$2a$10$gcUz6PGJLUrSt1TF6/rnc.FLqJPzevnkzDx2yY2Y49ehhuAxhQ4WW', 'USER_5', 'USER', NOW(), NOW()),
-       (6, 'user_6', '$2a$10$gcUz6PGJLUrSt1TF6/rnc.FLqJPzevnkzDx2yY2Y49ehhuAxhQ4WW', 'USER_6', 'USER', NOW(), NOW()),
-       (7, 'user_7', '$2a$10$gcUz6PGJLUrSt1TF6/rnc.FLqJPzevnkzDx2yY2Y49ehhuAxhQ4WW', 'USER_7', 'USER', NOW(), NOW()),
-       (8, 'user_8', '$2a$10$gcUz6PGJLUrSt1TF6/rnc.FLqJPzevnkzDx2yY2Y49ehhuAxhQ4WW', 'USER_8', 'USER', NOW(), NOW()),
-       (9, 'user_9', '$2a$10$gcUz6PGJLUrSt1TF6/rnc.FLqJPzevnkzDx2yY2Y49ehhuAxhQ4WW', 'USER_9', 'USER', NOW(), NOW()),
-       (10, 'user_10', '$2a$10$gcUz6PGJLUrSt1TF6/rnc.FLqJPzevnkzDx2yY2Y49ehhuAxhQ4WW', 'USER_10', 'USER', NOW(), NOW());
+VALUES (1, 'user_1', '$2a$10$gcUz6PGJLUrSt1TF6/rnc.FLqJPzevnkzDx2yY2Y49ehhuAxhQ4WW', '귀여운토끼', 'USER', NOW(), NOW()),
+       (2, 'user_2', '$2a$10$gcUz6PGJLUrSt1TF6/rnc.FLqJPzevnkzDx2yY2Y49ehhuAxhQ4WW', '용감한호랑이', 'USER', NOW(), NOW()),
+       (3, 'user_3', '$2a$10$gcUz6PGJLUrSt1TF6/rnc.FLqJPzevnkzDx2yY2Y49ehhuAxhQ4WW', '지혜로운부엉이', 'USER', NOW(), NOW()),
+       (4, 'user_4', '$2a$10$gcUz6PGJLUrSt1TF6/rnc.FLqJPzevnkzDx2yY2Y49ehhuAxhQ4WW', '빠른치타', 'USER', NOW(), NOW()),
+       (5, 'user_5', '$2a$10$gcUz6PGJLUrSt1TF6/rnc.FLqJPzevnkzDx2yY2Y49ehhuAxhQ4WW', '성실한개미', 'USER', NOW(), NOW()),
+       (6, 'user_6', '$2a$10$gcUz6PGJLUrSt1TF6/rnc.FLqJPzevnkzDx2yY2Y49ehhuAxhQ4WW', '재치있는여우', 'USER', NOW(), NOW()),
+       (7, 'user_7', '$2a$10$gcUz6PGJLUrSt1TF6/rnc.FLqJPzevnkzDx2yY2Y49ehhuAxhQ4WW', '강인한곰', 'USER', NOW(), NOW()),
+       (8, 'user_8', '$2a$10$gcUz6PGJLUrSt1TF6/rnc.FLqJPzevnkzDx2yY2Y49ehhuAxhQ4WW', '우아한백조', 'USER', NOW(), NOW()),
+       (9, 'user_9', '$2a$10$gcUz6PGJLUrSt1TF6/rnc.FLqJPzevnkzDx2yY2Y49ehhuAxhQ4WW', '영리한돌고래', 'USER', NOW(), NOW()),
+       (10, 'user_10', '$2a$10$gcUz6PGJLUrSt1TF6/rnc.FLqJPzevnkzDx2yY2Y49ehhuAxhQ4WW', '신속한매', 'USER', NOW(), NOW()),
+       (11, 'admin', '$2a$10$KfN5M7d9YNGfK/7oQkHzFeLeG6eFv.zbDHL7qN9fUbbdS3G6wOEqK', 'ADMIN', 'ADMIN', NOW(), NOW()),
+       (12, 'guest', '$2a$10$OrK25aG4HqYbXk4NHtMzoOPF9V2B3oFfjkqhv8o1cX1j8xFWXZoe2', 'GUEST', 'USER', NOW(), NOW());
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK
 TABLES;
@@ -57,12 +63,12 @@ LOCK
 TABLES `chat_room` WRITE;
 /*!40000 ALTER TABLE `chat_room` DISABLE KEYS */;
 INSERT INTO `chat_room` (`chat_room_id`, `title`, `max_capacity`, `created_at`, `updated_at`)
-VALUES (1, 'CHAT_ROOM_1', 10, NOW(), NOW()),
-       (2, 'CHAT_ROOM_2', 10, NOW(), NOW()),
-       (3, 'CHAT_ROOM_3', 10, NOW(), NOW()),
-       (4, 'CHAT_ROOM_4', 10, NOW(), NOW()),
-       (5, 'CHAT_ROOM_5', 10, NOW(), NOW()),
-       (6, 'CHAT_ROOM_6', 10, NOW(), NOW())
+VALUES (1, '노래 추천좀', 4, NOW(), NOW()),
+       (2, '점메추', 3, NOW(), NOW()),
+       (3, '재밌는 사람만', 10, NOW(), NOW()),
+       (4, '고민 들어줄 사람 있나요?', 2, NOW(), NOW()),
+       (5, '면접 준비 도와줄사람?', 3, NOW(), NOW()),
+       (6, '같이 노래방 갈사람 ㄱㄱ', 10, NOW(), NOW())
 ;
 
 /*!40000 ALTER TABLE `chat_room` ENABLE KEYS */;
@@ -73,18 +79,31 @@ LOCK
 TABLES `chat_room_member` WRITE;
 /*!40000 ALTER TABLE `chat_room_member` DISABLE KEYS */;
 INSERT INTO `chat_room_member` (`chat_room_member_id`, `chat_room_id`, `user_id`, `role`, `created_at`, `updated_at`)
-VALUES (1, 1, 1, 'ADMIN', NOW(), NOW()),
-       (2, 1, 2, 'MEMBER', NOW(), NOW()),
-       (3, 2, 1, 'MEMBER', NOW(), NOW()),
-       (4, 2, 2, 'ADMIN', NOW(), NOW()),
-       (5, 3, 1, 'MEMBER', NOW(), NOW()),
-       (6, 3, 2, 'MEMBER', NOW(), NOW()),
-       (7, 3, 3, 'ADMIN', NOW(), NOW()),
-       (8, 4, 1, 'MEMBER', NOW(), NOW()),
-       (9, 4, 2, 'MEMBER', NOW(), NOW()),
-       (10, 4, 3, 'MEMBER', NOW(), NOW()),
-       (11, 4, 4, 'ADMIN', NOW(), NOW())
-;
+VALUES
+-- 1번방: 노래 추천좀
+       (1, 1, 1, 'ADMIN', '2025-08-31 10:00:00', '2025-08-31 10:00:00'),
+       (2, 1, 4, 'MEMBER', '2025-08-31 10:01:00', '2025-08-31 10:01:00'),
+       (3, 1, 5, 'MEMBER', '2025-08-31 10:02:00', '2025-08-31 10:02:00'),
+-- 2번방: 점메추
+       (4, 2, 2, 'ADMIN', '2025-08-31 10:03:00', '2025-08-31 10:03:00'),
+       (5, 2, 6, 'MEMBER', '2025-08-31 10:04:00', '2025-08-31 10:04:00'),
+       (6, 2, 7, 'MEMBER', '2025-08-31 10:05:00', '2025-08-31 10:05:00'),
+-- 3번방: 재밌는 사람만
+       (7, 3, 3, 'ADMIN', '2025-08-31 10:06:00', '2025-08-31 10:06:00'),
+       (8, 3, 8, 'MEMBER', '2025-08-31 10:07:00', '2025-08-31 10:07:00'),
+       (9, 3, 9, 'MEMBER', '2025-08-31 10:08:00', '2025-08-31 10:08:00'),
+-- 4번방: 고민 들어줄 사람 있나요?
+       (10, 4, 4, 'ADMIN', '2025-08-31 10:09:00', '2025-08-31 10:09:00'),
+       (11, 4, 10, 'MEMBER', '2025-08-31 10:10:00', '2025-08-31 10:10:00'),
+       (12, 4, 11, 'MEMBER', '2025-08-31 10:11:00', '2025-08-31 10:11:00'),
+-- 5번방: 면접 준비 도와줄사람?
+       (13, 5, 1, 'ADMIN', '2025-08-31 10:12:00', '2025-08-31 10:12:00'),
+       (14, 5, 5, 'MEMBER', '2025-08-31 10:13:00', '2025-08-31 10:13:00'),
+       (15, 5, 6, 'MEMBER', '2025-08-31 10:14:00', '2025-08-31 10:14:00'),
+-- 6번방: 같이 노래방 갈사람 ㄱㄱ
+       (16, 6, 2, 'ADMIN', '2025-08-31 10:15:00', '2025-08-31 10:15:00'),
+       (17, 6, 7, 'MEMBER', '2025-08-31 10:16:00', '2025-08-31 10:16:00'),
+       (18, 6, 8, 'MEMBER', '2025-08-31 10:17:00', '2025-08-31 10:17:00');
 /*!40000 ALTER TABLE `chat_room_member` ENABLE KEYS */;
 UNLOCK
 TABLES;
@@ -93,19 +112,43 @@ LOCK
 TABLES `chat_message` WRITE;
 /*!40000 ALTER TABLE `chat_message` DISABLE KEYS */;
 INSERT INTO `chat_message` (`chat_message_id`, `chat_room_id`, `user_id`, `content`, `created_at`, `updated_at`)
-VALUES (1, 1, 1, '안녕하세요~', '2024-01-01 10:00:00', '2024-01-01 10:00:00'),
-       (2, 1, 2, '안녕하세요~', '2024-01-01 10:01:00', '2024-01-01 10:01:00'),
-       (3, 2, 1, '안녕하세요~', '2024-01-01 10:02:00', '2024-01-01 10:02:00'),
-       (4, 2, 2, '네 반갑습니다.', '2024-01-01 10:03:00', '2024-01-01 10:03:00'),
-       (5, 3, 1, '안녕하세요~', '2024-01-01 10:04:00', '2024-01-01 10:04:00'),
-       (6, 3, 2, '잘 부탁드립니다!', '2024-01-01 10:05:00', '2024-01-01 10:05:00'),
-       (7, 3, 3, '반갑습니다~', '2024-01-01 10:06:00', '2024-01-01 10:06:00'),
-       (8, 4, 1, '안녕하세요~', '2024-01-01 10:07:00', '2024-01-01 10:07:00'),
-       (9, 4, 2, '저는 백엔드입니다', '2024-01-01 10:08:00', '2024-01-01 10:08:00'),
-       (10, 4, 3, '저는 프론트입니다', '2024-01-01 10:09:00', '2024-01-01 10:09:00'),
-       (11, 4, 4, '저는 모바일입니다', '2024-01-01 10:10:00', '2024-01-01 10:10:00'),
-       (12, 4, 4, '저는 모바일입니다22', '2024-01-01 10:11:00', '2024-01-01 10:11:00')
-;
+VALUES
+-- 1번방: 노래 추천좀
+    (1, 1, 4, '요즘 듣기 좋은 노래 뭐 있어?', '2025-08-31 10:00:00', '2025-08-31 10:00:00'),
+    (2, 1, 5, '저는 뉴진스 노래 추천해요!', '2025-08-31 10:01:00', '2025-08-31 10:01:00'),
+    (3, 1, 1, '잔나비 노래도 좋아요!', '2025-08-31 10:02:00', '2025-08-31 10:02:00'),
+    (4, 1, 4, '혹시 발라드 추천해줄 사람?', '2025-08-31 10:03:00', '2025-08-31 10:03:00'),
+    (5, 1, 5, '폴킴 노래 들어보세요!', '2025-08-31 10:04:00', '2025-08-31 10:04:00'),
+
+-- 2번방: 점메추
+    (6, 2, 6, '오늘 점심 뭐 먹지?', '2025-08-31 10:05:00', '2025-08-31 10:05:00'),
+    (7, 2, 7, '김치찌개 어때요?', '2025-08-31 10:06:00', '2025-08-31 10:06:00'),
+    (8, 2, 2, '저는 샐러드 추천!', '2025-08-31 10:07:00', '2025-08-31 10:07:00'),
+
+-- 3번방: 재밌는 사람만
+    (9, 3, 8, '재밌는 얘기 해줄 사람?', '2025-08-31 10:08:00', '2025-08-31 10:08:00'),
+    (10, 3, 9, '어제 축구 봤어요? 완전 웃겼음!', '2025-08-31 10:09:00', '2025-08-31 10:09:00'),
+    (11, 3, 3, '밈 공유해요~', '2025-08-31 10:10:00', '2025-08-31 10:10:00'),
+    (12, 3, 8, '오늘 회사에서 있었던 썰 풀어봄', '2025-08-31 10:11:00', '2025-08-31 10:11:00'),
+
+-- 4번방: 고민 들어줄 사람 있나요?
+    (13, 4, 10, '요즘 진로 고민이 많아요...', '2025-08-31 10:12:00', '2025-08-31 10:12:00'),
+    (14, 4, 11, '무슨 고민인지 말해봐요!', '2025-08-31 10:13:00', '2025-08-31 10:13:00'),
+    (15, 4, 4, '저도 비슷한 고민 있었어요', '2025-08-31 10:14:00', '2025-08-31 10:14:00'),
+
+-- 5번방: 면접 준비 도와줄사람?
+    (16, 5, 5, '면접 준비 어떻게 해야 할까요?', '2025-08-31 10:15:00', '2025-08-31 10:15:00'),
+    (17, 5, 6, '자기소개 연습해봤어요?', '2025-08-31 10:16:00', '2025-08-31 10:16:00'),
+    (18, 5, 1, '기술 면접 팁 공유해요!', '2025-08-31 10:17:00', '2025-08-31 10:17:00'),
+    (19, 5, 5, '면접 질문 리스트 있어요?', '2025-08-31 10:18:00', '2025-08-31 10:18:00'),
+
+-- 6번방: 같이 노래방 갈사람 ㄱㄱ
+    (20, 6, 7, '노래방 언제 갈까요?', '2025-08-31 10:19:00', '2025-08-31 10:19:00'),
+    (21, 6, 8, '주말에 시간 되는 사람?', '2025-08-31 10:20:00', '2025-08-31 10:20:00'),
+    (22, 6, 2, '노래방에서 부를 곡 추천해요!', '2025-08-31 10:21:00', '2025-08-31 10:21:00'),
+    (23, 6, 7, '2차로 밥도 먹을래요?', '2025-08-31 10:22:00', '2025-08-31 10:22:00'),
+    (24, 6, 8, '노래방 예약은 누가 할까요?', '2025-08-31 10:23:00', '2025-08-31 10:23:00'),
+    (25, 6, 2, '다들 어떤 노래 좋아해요?', '2025-08-31 10:24:00', '2025-08-31 10:24:00');
 /*!40000 ALTER TABLE `chat_message` ENABLE KEYS */;
 UNLOCK
 TABLES;
